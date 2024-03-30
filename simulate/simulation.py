d46928b7187a274ef781ad4c0acc8a0d1752e78d
plarotta <plarottaone2@gmail.com>
2024-03-30 00:22:36 -0400

    added saving support to simulator

--- simulate/simulation.py
+++ simulate/simulation.py
@@ -1,6 +1,9 @@
 import numpy as np
 from fast_utils import *
 from tqdm import tqdm
+import pickle
+import os
+from datetime import datetime
 
 
 class Simulation():
@@ -46,7 +49,7 @@ class Simulation():
     def interact(self, springs, masses, t):
         interact_fast(springs, masses, t, self.time_step, self.mu_s, self. mu_k, self.floor_z_pos)
 
-    def run_simulation(self, sim_length, time_step, log_k, mu_s, mu_k, floor_z_position):   
+    def run_simulation(self, sim_length, time_step, log_k, mu_s, mu_k, floor_z_position, save=False):   
         for t in tqdm(np.arange(0, sim_length, time_step)):
             if t % log_k == 0:
                 p1 = self.get_COM(self.masses)
@@ -61,8 +64,16 @@ class Simulation():
                 }
             else:
                 interact_fast(self.springs, self.masses, t, time_step, mu_s, mu_k, floor_z_position)
+        if save is True:
+            path = os.getcwd()+'/simulate/saved_logs'
+            now = datetime.now()
+            now = f'{now.month}-{now.day}-{now.year}-{now.hour}-{now.minute}-{now.second}.pkl'
+
+            with open(os.path.join(path, now), 'wb') as handle:
+                pickle.dump(self.sim_log, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
 
 if __name__ == "__main__":
     simy = Simulation()
-    simy.run_simulation(300, .001, 3, 0.9, 0.8, -4)
+    simy.run_simulation(300, .001, 10, 0.9, 0.8, -4, save=True)
         
