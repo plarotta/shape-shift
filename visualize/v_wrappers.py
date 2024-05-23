3de8afccd40bed1dc537bd4e0d963e314a999ff9
plarotta <plarottaone2@gmail.com>
2024-05-22 22:59:13 -0400

    another visualization checkpoint

--- visualize/v_wrappers.py
+++ visualize/v_wrappers.py
@@ -8,6 +8,7 @@ import os
 sys.path.append(os.getcwd())
 from simulate.fast_utils import get_COM_fast, get_spring_l_fast
 import pysim
+import numpy as np
 
 
 def initialize_scene(z):
@@ -20,57 +21,43 @@ def initialize_scene(z):
     for y in range(-50,53,3):
         gridx = curve(pos=[vector(50,z-0.06,y),vector(-50,z-0.06,y)],color=color.black)
 
-def plot(masses):
-    m_plots = []
-    
-    for mass in masses:
-        m_temp = sphere(pos=vector(mass[0][0],mass[0][1],mass[0][2]), 
-                        radius=0.06, 
-                        color=color.green)
-        m_plots.append(m_temp)
-    COM_pos = get_COM_fast(masses)
-    
-    COM = sphere(pos=vector(COM_pos[0],COM_pos[1],COM_pos[2]), radius=0.06, color=color.yellow)
-    scene.camera.follow(COM)
-
-def plot_springs(springs, masses):
-    # s_plots = []
-    for spring in springs:
-        m1_pos = masses[int(spring[0][0])][0]
-        m2_pos = masses[int(spring[0][1])][0]
-        s_temp = cylinder(pos=vector(m1_pos[0], m1_pos[1], m1_pos[2]), 
-                          axis=vector(m2_pos[0]-m1_pos[0], m2_pos[1]-m1_pos[1], m2_pos[2]-m1_pos[2]), 
-                          length=get_spring_l_fast(masses,spring),
-                          color=color.red, 
-                          radius=0.01)
-        # s_plots.append(s_temp)
-    
-    
-    # return(m_plots,s_plots,COM,v)
-
-def plot_robot_masses(robot_masses, offset_x=0,offset_z=0):
-    mass_plots = []
-    for mass in robot_masses:
-        m_temp = sphere(pos=vector(mass[0]+offset_x,mass[1],mass[2]+offset_z), 
-                        radius=0.06, 
-                        color=color.green)
-        mass_plots.append(m_temp)
-    COM_pos = get_COM_fast(robot_masses)
-    COM_plots = sphere(pos=vector(COM_pos[0],COM_pos[1],COM_pos[2]), radius=0.06, color=color.yellow)
+def plot_robot_masses(robot_masses, mass_plots=None, COM_plot=None, offset_x=0,offset_z=0):
+    if mass_plots is None:
+        mass_plots = []
+        for mass in robot_masses:
+            m_temp = sphere(pos=vector(mass[0]+offset_x,mass[1],mass[2]+offset_z), 
+                            radius=0.06, 
+                            color=color.green)
+            mass_plots.append(m_temp)
+        COM_pos = get_COM_fast(robot_masses)
+        COM_plots = sphere(pos=vector(COM_pos[0],COM_pos[1],COM_pos[2]), radius=0.06, color=color.yellow)
+    else:
+        for idx,m in enumerate(mass_plots):
+            m.pos = vector(robot_masses[idx][0], robot_masses[idx][1], robot_masses[idx][2])
+        COM_pos = get_COM_fast(robot_masses)
+        COM_plot.pos = vector(COM_pos[0],COM_pos[1],COM_pos[2])
     return(mass_plots, COM_plots)
 
-def plot_robot_springs(robot_springs, robot_masses, offset_x=0, offset_y=0):
-    s_plots = []
-    for spring in robot_springs:
-        m1_pos = robot_masses[int(spring[0])][0:3]
-        m2_pos = robot_masses[int(spring[1])][0:3]
-        s_temp = cylinder(pos=vector(m1_pos[0], m1_pos[1], m1_pos[2]), 
-                          axis=vector(m2_pos[0]-m1_pos[0], m2_pos[1]-m1_pos[1], m2_pos[2]-m1_pos[2]), 
-                          length=get_spring_l_fast(robot_masses,spring),
-                          color=color.red, 
-                          radius=0.01)
-        s_plots.append(s_temp)
-
+def plot_robot_springs(robot_springs, robot_masses, spring_plots=None, offset_x=0, offset_y=0):
+    if spring_plots is None:
+        spring_plots = []
+        for spring in robot_springs:
+            m1_pos = robot_masses[int(spring[0])][0:3]
+            m2_pos = robot_masses[int(spring[1])][0:3]
+            s_temp = cylinder(pos=vector(m1_pos[0], m1_pos[1], m1_pos[2]), 
+                            axis=vector(m2_pos[0]-m1_pos[0], m2_pos[1]-m1_pos[1], m2_pos[2]-m1_pos[2]), 
+                            length=get_spring_l_fast(robot_masses,spring),
+                            color=color.red, 
+                            radius=0.01)
+            spring_plots.append(s_temp)
+    else:
+        for idx,s in enumerate(spring_plots):
+            m1_pos = robot_masses[int(robot_springs[idx][0])][0:3]
+            m2_pos = robot_masses[int(robot_springs[idx][1])][0:3]
+            s.pos = vector(m1_pos[0], m1_pos[1], m1_pos[2])
+            s.axis = vector(m2_pos[0]-m1_pos[0], m2_pos[1]-m1_pos[1], m2_pos[2]-m1_pos[2])
+            s.length= get_spring_l_fast(robot_masses,robot_springs[idx])
+    return(spring_plots)
 
 def update_scene(masses, springs, m_plots, s_plots, COM,v,speed):
     for idx,m in enumerate(m_plots):
@@ -85,6 +72,15 @@ def update_scene(masses, springs, m_plots, s_plots, COM,v,speed):
     COM_pos = get_COM_fast(masses)
     COM.pos = vector(COM_pos[0],COM_pos[1],COM_pos[2])
     v.text = str(str(round(speed,3))+" m/s")
+
+def update_viz(sim_masses, sim_springs, mass_plots, spring_plots, COM_plots, velocity_plots, speed_plots):
+    if len(sim_masses) == 1:
+        mass_plots, COM_plots = plot_robot_masses(sim_masses[0], mass_plots, COM_plots)
+        spring_plots = plot_robot_springs(sim_springs[0], sim_masses[0], spring_plots)
+    else:
+        # TODO: generalize for N robots (need offsets so the robots aren't plotted on top of each other)
+        pass
+
 
 def visualize_simulation(path_to_sim,floor):
     time.sleep(6)
@@ -108,14 +104,34 @@ def visualize_simulation(path_to_sim,floor):
 
 if __name__ == "__main__":
     
-    # from simulate.fast_utils import *
-    # from simulate.simulation import *
     simy = pysim.Simulation()
+
+    # set the scene
     initialize_scene(0)
-    plot_robot_masses(simy.get_sim_masses()[0])
-    plot_robot_springs(simy.get_sim_springs()[0], simy.get_sim_masses()[0])
+    m_ob, com_ob = plot_robot_masses(simy.get_sim_masses()[0])
+    s_ob = plot_robot_springs(simy.get_sim_springs()[0], simy.get_sim_masses()[0])
+    sim_length = 1
+    time_step = 0.1
+    for i,t in enumerate(np.arange(0, sim_length, time_step)):
+        rate(50)
+
+    # # if i % log_k == 0:
+    # #     # p1 = self.get_COM(self.masses)
+    # #     sim.step(t)
+    # #     # p2 = self.get_COM(self.masses)
+    # #     # displacement = np.linalg.norm(np.array([p2[0],0,p2[2]])-np.array([p1[0],0,p1[2]]))
+    # #     # speed = displacement/time_step
+    # #     # self.sim_log[t] = {
+    # #     #     'masses': np.copy(self.masses),
+    # #     #     'springs': np.copy(self.springs),
+    # #     #     'speed': speed
+    # #     # }
+    # # else:
+    # #     sim.step(t)
+    #     sim.step()
+    #     print(f'SIM TIME: {sim.get_sim_t()}')
+    #     print(len(sim.get_sim_masses()))
+    #     # print(sim.get_sim_springs())
+    #     input()
 
     
-    # a = Simulation()
-    # sim_path = a.run_simulation(sim_length=20, time_step=0.001, log_k=5, mu_s=0.5, mu_k=0.3, floor_z_position=-4, save=True)
-    # visualize_simulation(sim_path, floor=-4)
